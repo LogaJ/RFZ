de94b755bdef879c21774c1fa6cac29848aa10db
LogiBogi <loga.jegede+github@gmail.com>
2013-02-09 13:03:08 +0000

    let's vc this puppy.

---
+++ app/models/zombie.rb
@@ -0,0 +1,3 @@
+class Zombie < ActiveRecord::Base
+  attr_accessible :age, :bio, :name, :rotting
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterForZombies::Application.config.secret_token = '30917ed34cc920d6957c4bc07edae38db68b8db64f6ec57a4b5095a29b705392c1c04a90d9fedc60404573da1a055f0c6e250172a059128e5fc5d171489c05e0'
